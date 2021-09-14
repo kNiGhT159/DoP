503d07a70740b7d391561f37041cabc1eb2c2bf1
Ronak <ronakdesai159@gmail.com>
2021-09-14 22:35:53 +0530

    2

--- fibonacci.java
+++ fibonacci.java
@@ -2,12 +2,12 @@ public class fibonacci
 {
     public static void main(String args[]) 
     {
-        int n, a = 0, b = 0, c = 1;
-        n = Integer.parseInt(args[0]);
+        int num, a = 0, b = 0, c = 1;
+        num = Integer.parseInt(args[0]);
         
         System.out.print("Fibonacci Series : ");
         
-        for(int i = 1; i <= n; i++)
+        for(int i = 1; i <= num; i++)
         {
             a = b;
             b = c;
